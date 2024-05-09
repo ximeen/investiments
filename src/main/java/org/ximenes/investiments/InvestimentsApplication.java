cf89491c4dd5c0636b314862d9d08ba4d0a7f778
Gabriel Ximenes <swg-ximenes@iMac-Pro-de-Gabriel.local>
2024-05-09 20:08:40 -0300

    first commit

---
+++ src/main/java/org/ximenes/investiments/InvestimentsApplication.java
@@ -0,0 +1,13 @@
+package org.ximenes.investiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ximenes/investiments/InvestimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ximenes.investiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
